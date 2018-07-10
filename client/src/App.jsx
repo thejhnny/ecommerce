1cd69df2e9670ff514b39efa79b56a48ca245e8c
Johnny Siu <siujohnny@gmail.com>
2018-07-10 13:07:30 -0700

    setting up

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>
+        <h3> E COMMERCE APP </h3>
+        </div>
+    )
+  }
+}
+
+export default App;
